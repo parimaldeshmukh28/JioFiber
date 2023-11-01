2123bceebbc0043f8a885d6eedad91e336b166d8
parimal123 <parimal.d.deshmukh>
2023-11-01 11:49:02 +0530

    reliance internet service

---
+++ src/main/java/com/reliance/jiofiber/JiofiberApplication.java
@@ -0,0 +1,13 @@
+package com.reliance.jiofiber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiofiberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiofiberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reliance/jiofiber/JiofiberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reliance.jiofiber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiofiberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
